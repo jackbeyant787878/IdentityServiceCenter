b428f3c6e3d2d83b7f9aa78b03c9721ff6ddbe4f
jackb <jackb@WIN-TOH4A3L6TPC>
2026-06-20 21:37:30 +0800

    add health check

---
+++ IdentityService.Auth.HttpApi/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityService.Auth.HttpApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Healthy");
+
+    }
+}
